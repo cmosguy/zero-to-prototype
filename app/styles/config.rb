6cb6f717c7188185adbc98678818ddcaac22899d
Adam Klein <asklein@gmail.com>
2015-05-13 14:43:46 -0700

    updated with latest from Ryan

---
+++ app/styles/config.rb
@@ -0,0 +1,12 @@
+extensions_dir = "bower_components/gumby/sass/extensions"
+
+Compass::Frameworks.register('modular-scale', :path => File.expand_path("#{extensions_dir}/modular-scale"))
+
+http_path = "/"
+
+css_dir = "css"
+
+images_dir = "img"
+
+sass_dir = "sass"
+
